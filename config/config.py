ba3d4dd868de8154d7d6f53d5f0d0c0f15561181
Sam_x_developer <129406748+Samdaveloper@users.noreply.github.com>
2023-04-13 20:37:34 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "5864436910").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://t.me/lover_jerry")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Samdaveloper/DILxMUSIC123")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
