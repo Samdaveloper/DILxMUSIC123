36536df877f37c1679108ba433e164408ac78285
Sam_x_developer <129406748+Samdaveloper@users.noreply.github.com>
2023-04-13 17:08:05 +0530

    Update song.py

--- DILX/utils/inline/song.py
+++ DILX/utils/inline/song.py
@@ -15,7 +15,7 @@ def song_markup(_, vidid):
         ],
         [
             InlineKeyboardButton(
-                text="❣️sᴜᴩᴩᴏʀᴛ[ᴅɪʟ[❣️]", url=f"{config.SUPPORT_GROUP}",
+                text="❣️sᴜᴩᴩᴏʀᴛ[✘𓂆𓂇Đ[❣️]", url=f"{config.SUPPORT_GROUP}",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
